2226a165baa7941023e6a8fd5b100d2fa00b8e30
hacknus <l_stoeckli@bluewin.ch>
2021-10-20 17:08:58 +0200

    sort chores implemented

--- python/server.py
+++ python/server.py
@@ -3,6 +3,7 @@ from websocket_server import WebsocketServer
 import json
 from chore import Chore
 from datetime import datetime, timedelta
+import operator
 
 
 def new_client(client, server):
@@ -21,28 +22,37 @@ def new_msg(client, server, message):
     server.send_message_to_all("ok")
 
 
-def init():
-    file = open("state.json")
-    d = json.load(file)
-    chores = []
-    for c in d["chores"]:
-        next_date = datetime.now() + timedelta(days=c["countdown"])
-        chores.append(Chore(c["name"], c["period"], next_date, c["who"]))
-    for c in d["timedChores"]:
-        next_date = c["last"]
-        while next_date + timedelta(days=c["period"]) < datetime.now():
-            # condition does not work with < operator
-            next_date = next_date + timedelta(days=c["period"])
-        chores.append(Chore(c["name"], c["period"], next_date, c["who"], True))
+class Organiser:
+    def __init__(self):
+        file = open("state.json")
+        d = json.load(file)
+        self.chores = []
+        for c in d["chores"]:
+            next_date = datetime.now().date() + timedelta(days=c["countdown"])
+            self.chores.append(Chore(c["name"], c["period"], next_date, c["who"]))
+        for c in d["fixedChores"]:
+            next_date = datetime.strptime(c["last"], '%d-%m-%Y').date()
+            while next_date + timedelta(days=c["period"]) < datetime.now().date():
+                next_date = next_date + timedelta(days=c["period"])
+            self.chores.append(Chore(c["name"], c["period"], next_date, c["who"], True))
+
+    def get_due_today(self):
+        due_today = []
+        for chore in self.chores:
+            if chore.next_date <= datetime.today().date():
+                print(f"{chore.name} is due today")
+                due_today.append(chore)
+        due_today = self.sort(due_today)
+        return due_today
+
+    def sort(self, chores):
+        chores.sort(key=operator.attrgetter('next_date'))
+        return chores
 
 
-def main():
-    # if new day
-    # update all chores
-    pass
-
-
-init()
+planner = Organiser()
+due_today = planner.get_due_today()
+print(due_today)
 
 server = WebsocketServer(13254, host="0.0.0.0", loglevel=logging.INFO)
 server.set_fn_new_client(new_client)
